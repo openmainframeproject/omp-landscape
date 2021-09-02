0ffa8c0126afea255a028916de468ee516171b88
John Mertic <jmertic@linuxfoundation.org>
2021-09-02 11:03:40 -0400

    Add IBM Db2 Administration Foundation for z/OS (App Framework ZOWE V1) (#245)
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- make_ibm_logos.sh
+++ make_ibm_logos.sh
@@ -3,8 +3,8 @@
 cd $(dirname $BASH_SOURCE)
 cd hosted_logos
 
-logo='international_business_machines_corporation_ibm.svg'
-products=('IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
+logo='international_business_machines_corporation.svg'
+products=('IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
 
 for i in ${!products[@]};
 do
