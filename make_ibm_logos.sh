8dd990dca656fa8ab0909d82e022fcbd096f88d2
John Mertic <jmertic@linuxfoundation.org>
2021-11-16 16:14:56 -0500

    Add IBM RSE
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- make_ibm_logos.sh
+++ make_ibm_logos.sh
@@ -4,7 +4,7 @@ cd $(dirname $BASH_SOURCE)
 cd hosted_logos
 
 logo='international_business_machines_corporation.svg'
-products=('IBM z Open Editor' 'Zowe Explorer for IBM CICS' 'IBM Z Distribution for Zowe' 'IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
+products=('IBM RSE API Plug-in' 'IBM z Open Editor' 'Zowe Explorer for IBM CICS' 'IBM Z Distribution for Zowe' 'IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
 
 for i in ${!products[@]};
 do
