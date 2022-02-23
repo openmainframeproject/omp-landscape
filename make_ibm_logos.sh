da41572fc934826e644e080a4504e15e64fe0a96
John Mertic <jmertic@linuxfoundation.org>
2022-02-23 11:27:08 -0500

    IBM z/OS Workload Interaction Navigator (App Framework ZOWE V1) (#450)
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>
    Co-authored-by: CNCF-Bot <info@cncf.io>

--- make_ibm_logos.sh
+++ make_ibm_logos.sh
@@ -4,7 +4,7 @@ cd $(dirname $BASH_SOURCE)
 cd hosted_logos
 
 logo='international_business_machines_corporation.svg'
-products=('Z ChatOps' 'IBM Z JCL Expert' 'IBM RSE API Plug-in' 'IBM z Open Editor' 'Zowe Explorer for IBM CICS' 'IBM Z Distribution for Zowe' 'IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
+products=('Z ChatOps' 'IBM z/OS Workload Interaction Navigator' 'IBM Z JCL Expert' 'IBM RSE API Plug-in' 'IBM z Open Editor' 'IBM Z Distribution for Zowe' 'IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy')
 
 for i in ${!products[@]};
 do
@@ -16,7 +16,7 @@ do
   filename="${filename//(/}"
   filename="${filename//)/}"
   filename="${filename//™/}.svg"
-  texttoadd="<text x=\"50%\" y=\"240\" style=\"font: 22px sans-serif;alignment-baseline: middle; text-anchor:middle;\">${product}</text></svg>"
+  texttoadd="<text x=\"50%\" y=\"240\" style=\"font: 16px sans-serif;alignment-baseline: middle; text-anchor:middle;\">${product}</text></svg>"
   echo "Creating ${filename} for ${product}"
   cp $logo $filename
   sed -i'.original' 's/viewBox="0 0 400 245"/viewBox="0 0 400 300"/g' $filename
