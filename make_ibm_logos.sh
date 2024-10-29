b50db446aa95363d698b3a18cdc14c279561a5c0
John Mertic <jmertic@linuxfoundation.org>
2024-10-29 12:00:07 -0400

    Add zvm_ansible
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- make_ibm_logos.sh
+++ make_ibm_logos.sh
@@ -4,7 +4,41 @@ cd $(dirname $BASH_SOURCE)
 cd hosted_logos
 
 logo='international_business_machines_corporation.svg'
-products=('Z ChatOps' 'IBM z/OS Workload Interaction Navigator' 'IBM Z JCL Expert' 'IBM z Open Editor' 'IBM Z Distribution for Zowe' 'IBM® Db2 Administration Foundation for z/OS' 'IBM® Remote System Explorer API' 'crc32-s390x' 'IBM® Z System Automation' 'IBM® Z Service Management Explorer' 'IBM® Z Workload Scheduler' 'Db2 DevOps Experience for z/OS' 'Service Automation Suite' 'Service Management Suite for z/OS' 'Service Management Unite' 'IBM® Z Netview' 'libica' 'qclib' 's390-tools' 'Shared Memory Communication Tools' 'snIPL' 'source VIPA' 'System Loader (sysload)' 'z/OS Connect EE' 'z/OS Node Accessor' 'z/OS Tools' 'zfcp HBA API library' 'Remote System Explorer API' 'Zowe CLI CICS Deploy' 'IBM IMS Administration Foundation for z/OS' 'IBM Unified Management Server (UMS) for z/OS' 'IBM® z/OS FTP for Zowe Explorer')
+products=(
+  'Installation assistant for Linux on IBM Z'
+  'Z ChatOps'
+  'IBM z/OS Workload Interaction Navigator'
+  'IBM Z JCL Expert'
+  'IBM z Open Editor'
+  'IBM Z Distribution for Zowe'
+  'IBM® Db2 Administration Foundation for z/OS'
+  'IBM® Remote System Explorer API' 
+  'crc32-s390x' 
+  'IBM® Z System Automation' 
+  'IBM® Z Service Management Explorer' 
+  'IBM® Z Workload Scheduler' 
+  'Db2 DevOps Experience for z/OS' 
+  'Service Automation Suite' 
+  'Service Management Suite for z/OS' 
+  'Service Management Unite' 
+  'IBM® Z Netview' 
+  'libica' 
+  'qclib' 
+  's390-tools' 
+  'Shared Memory Communication Tools' 
+  'snIPL' 
+  'source VIPA' 
+  'System Loader (sysload)' 
+  'z/OS Connect EE' 
+  'z/OS Node Accessor' 
+  'z/OS Tools' 
+  'zfcp HBA API library' 
+  'Zowe CLI CICS Deploy' 
+  'IBM IMS Administration Foundation for z/OS' 
+  'IBM Unified Management Server (UMS) for z/OS' 
+  'IBM® z/OS FTP for Zowe Explorer'
+  'zvm_ansible'
+)
 
 for i in ${!products[@]};
 do
