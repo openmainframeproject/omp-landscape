1707a38ac068f2e1959e4356f6331b547c73c00f
John Mertic <jmertic@linuxfoundation.org>
2026-02-12 15:55:43 -0500

    Move the scripts under scripts (#1038)
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- scripts/make_rocket_software_logos.sh
+++ scripts/make_rocket_software_logos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd $(dirname $BASH_SOURCE)
-cd hosted_logos
+cd "$(dirname "$BASH_SOURCE")" || exit 1
+cd ../hosted_logos || exit 1
 
 logo='rocket_software_inc.svg'
 products=('Rocket® PRO/JCL' 'Rocket™ BlueZone Web' 'Rocket Support for Zowe')
